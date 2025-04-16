7b5f10b513c1c738dfd09d4c8635701290b4b999
GideonWhite1029 <gideonwhite1029@gmail.com>
2025-04-16 14:26:24 +0300

    Update Upstream (Purpur)

--- horizon-server/src/main/java/dev/gideonwhite1029/horizon/protocol/core/HorizonProtocolManager.java
+++ horizon-server/src/main/java/dev/gideonwhite1029/horizon/protocol/core/HorizonProtocolManager.java
@@ -6,7 +6,7 @@ import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
-import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.jetbrains.annotations.NotNull;
 
